499b22757a291247ee0a6393251ea6d99830be30
pank-su <pank@pank.su>
2025-09-30 14:42:16 +0300

    :sparkles: выполнение л/р 1

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins{
+    application
+    java
+}
+
+val lwjglVersion = "3.3.6"
+val lwjglNatives = "natives-linux"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation ("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+}
+
+application {
+    mainClass = "su.pank.lab1.App"
+}
---
+++ src/main/java/su/pank/lab1/App.java
@@ -0,0 +1,42 @@
+package su.pank.lab1;
+
+import org.lwjgl.opengl.GL;
+
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11C.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+class App{
+    public static void main(String[] args){
+        long window;
+        if (!glfwInit()){
+            throw new IllegalStateException("Не удаётся запустить GLFW");
+        }
+        window = glfwCreateWindow(640, 480, "Панков", NULL, NULL);
+        if (window == NULL){
+            glfwTerminate();
+            throw new RuntimeException("Окно не создалось");
+        }
+        glfwSetWindowPos(window, 50, 50);
+        glfwMakeContextCurrent(window);
+        GL.createCapabilities();
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glBegin(GL_POLYGON);
+            glColor3f(1, 0, 0);
+            glVertex3f(-0.6f, -0.75f, 0.5f);
+            glColor3f(0, 1, 0);
+            glVertex3f(0.6f, -0.75f, 0.5f);
+            glColor3f(0, 0, 1);
+            glVertex3f(0f, 0.75f, 0f);
+            glEnd();
+
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+}
